4716337943d666792f41303f6c769bf96f1ce9ef
POMBNK <shardex11@gmail.com>
2023-08-25 18:37:47 +0300

    feat:Updated sql schema

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 CREATE TABLE users (
-    id serial PRIMARY KEY NOT NULL
+    id serial PRIMARY KEY NOT NULL,
+    name varchar(50),
+    email varchar(50)
 );
 
 CREATE TABLE segment (
@@ -21,3 +23,16 @@ CREATE TABLE "user_segment" (
 ALTER TABLE user_segment ADD FOREIGN KEY (user_id) REFERENCES users (id);
 
 ALTER TABLE user_segment ADD FOREIGN KEY (segment_id) REFERENCES segment (id);
+
+-- users stubs
+INSERT INTO users (name, email) VALUES ('Ilya','Il@ya.ru');
+INSERT INTO users (name, email) VALUES ('Vitya','Vit@ya.ru');
+INSERT INTO users (name, email) VALUES ('Kolya','Kol@ya.ru');
+INSERT INTO users (name, email) VALUES ('Katya','k@ya.ru');
+INSERT INTO users (name, email) VALUES ('pbInya','pbIn@ya.ru');
+
+--segment stubs
+INSERT INTO segment (name, active) VALUES ('discount30','1');
+INSERT INTO segment (name, active) VALUES ('discount50','1');
+INSERT INTO segment (name, active) VALUES ('discount80','1');
+INSERT INTO segment (name, active) VALUES ('voice_msg','1');
